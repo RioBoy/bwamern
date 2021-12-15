2c23196aa23b80bae2ed4bfeea5ba5dadc096583
RioBoy <boyrio68@gmail.com>
2021-12-15 23:08:31 +0700

    slicing testimonial from figma to react component

--- src/pages/landingPage.js
+++ src/pages/landingPage.js
@@ -4,6 +4,7 @@ import Header from 'parts/Header';
 import Hero from 'parts/Hero';
 import MostPicked from 'parts/MostPicked';
 import Categories from 'parts/Categories';
+import Testimoni from 'parts/Testimoni';
 
 import landingPage from 'json/landingPage.json';
 export default class LandingPage extends Component {
@@ -21,6 +22,7 @@ export default class LandingPage extends Component {
           data={landingPage.mostPicked}
         />
         <Categories data={landingPage.categories} />
+        <Testimoni data={landingPage.testimonial} />
       </>
     );
   }
