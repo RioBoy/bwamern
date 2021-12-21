a0d4140bf3f2054033f3a46d892e1c1f10df8a96
RioBoy <boyrio68@gmail.com>
2021-12-22 01:14:33 +0700

    slicing checkout page from figma to react component

--- src/elements/Stepper/index.js
+++ src/elements/Stepper/index.js
@@ -6,24 +6,29 @@ export default function Stepper(props) {
   const { steps, initialStep } = props;
   const stepKeys = Object.keys(steps);
 
-  const [CurrentSep, setCurrentStep] = useState(
+  const [CurrentStep, setCurrentStep] = useState(
     stepKeys.indexOf(initialStep) > -1 ? initialStep : stepKeys[0],
   );
   const totalStep = stepKeys.length;
-  const indexStep = stepKeys.indexOf(CurrentSep);
+  const indexStep = stepKeys.indexOf(CurrentStep);
 
   function prevStep() {
     if (+indexStep > 0) setCurrentStep(stepKeys[indexStep - 1]);
   }
 
   function nextStep() {
-    if (+indexStep < totalStep) setCurrentStep(stepKeys[indexStep] + 1);
+    if (+indexStep < totalStep) setCurrentStep(stepKeys[indexStep + 1]);
   }
 
-  return <>{props.children(prevStep, nextStep, CurrentSep, steps)}</>;
+  return <>{props.children(prevStep, nextStep, CurrentStep, steps)}</>;
 }
 
 Stepper.propTypes = {
   data: propTypes.object.isRequired,
   initialStep: propTypes.string,
 };
+
+export { default as Numbering } from './Numbering';
+export { default as Meta } from './Meta';
+export { default as Controller } from './Controller';
+export { default as MainContent } from './MainContent';
